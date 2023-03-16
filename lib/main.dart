5188a7a659add449acf4694126b05add2f90cda0
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2023-03-16 16:58:03 +0700

    ulang

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_api/ui/pages/home.dart';
 
-import './routes/app_pages.dart';
-import './pages/home_page.dart';
+void main() => runApp(MyApis());
 
-import './controllers/usersC.dart';
+class MyApis extends StatelessWidget {
+  const MyApis({super.key});
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  final userC = Get.put(UsersC());
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: HomePage(),
-      getPages: AppPages.pages,
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
