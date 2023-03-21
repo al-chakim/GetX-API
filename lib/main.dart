90517d69590b786e319b3f1a25b359c2fc81d9fd
Muchammad Amru Al-Chakimi <amrualchakim@gmail.com>
2023-03-22 00:10:38 +0700

    lanjut

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_api/controllers/dataC.dart';
-import 'package:getx_api/routes/page.dart';
 
-void main() => runApp(MyApis());
+void main() => runApp(MyApp());
 
-class MyApis extends StatelessWidget {
-  final dataC = Get.put(DatasC());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      getPages: AppPages.pages,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Home Page',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(() => Dua());
+              },
+              child: const Text('Next Page >>'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dua extends StatelessWidget {
+  const Dua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Second Page',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(() => Tiga());
+              },
+              child: const Text('Next Page >>'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Tiga extends StatelessWidget {
+  const Tiga({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Third Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Third Page',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Next Page >>'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
